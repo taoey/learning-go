98ffac033e8b5994f3b8c4addab411ad4fc52119
taoey <741494582@qq.com>
2019-10-27 15:02:43 +0800

    FEATURE 添加Http-client相关

---
+++ src/base/http/http_client_test.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"testing"
+)
+
+// 一个简单的HttpClient-get请求
+func TestHttpClientGet(t *testing.T) {
+	resp, err := http.Get("https://www.baidu.com")
+	fmt.Println(resp,err)
+}
