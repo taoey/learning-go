233f598f9608a20381a60a04897751c254e8c093
taoey <741494582@qq.com>
2019-11-25 18:20:03 +0800

    FEATURE 添加协程池（需要进行改进）

---
+++ src/base/goroutine/goroutine_pool_test.go
@@ -0,0 +1,33 @@
+package goroutine
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+var pool = NewGoroutinePool()
+
+func TestGoroutinePool01(t *testing.T) {
+	pool.Run(&ConvertTask{
+		CallBack: func() {
+			time.Sleep(time.Second*10)
+			fmt.Println("hello1")
+
+		},
+	})
+
+	pool.Run(&ConvertTask{
+		CallBack: func() {
+			fmt.Println("hello2")
+		},
+	})
+
+	pool.Run(&ConvertTask{
+		CallBack: func() {
+			fmt.Println("hello3")
+		},
+	})
+
+	time.Sleep(time.Second*60)
+}
