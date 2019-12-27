cffb157145356c4a7bff4f382f9c006b758d173a
taoey <741494582@qq.com>
2019-12-27 10:12:27 +0800

    FEATURE 添加rabbitmq-hello world 程序

---
+++ src/third_part_lib/rabbitmq/rabbitmq_test.go
@@ -0,0 +1,100 @@
+package rabbitmq
+
+import (
+	"github.com/streadway/amqp"
+	"log"
+	"testing"
+)
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+	}
+}
+
+// send
+func Test01(t *testing.T) {
+
+	// ------------- 请求连接 ----------------------
+	// URL解析：amqp://{用户名}:{密码}@{ip}:{port}/{虚拟主机vhost}
+	conn, err := amqp.Dial("amqp://admin:admin@47.98.165.192:5672/my_vhost")
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	// -------------- 创建通道 ----------------------
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+
+	// ------------- 声明队列 ---------------------
+	q, err := ch.QueueDeclare(
+		"hello", // name
+		false,   // durable
+		false,   // delete when unused
+		false,   // exclusive
+		false,   // no-wait
+		nil,     // arguments
+	)
+	failOnError(err, "Failed to declare a queue")
+
+
+	// ------------ 发送消息 ---------------------
+	body := "Hello World!"
+	err = ch.Publish(
+		"",     // exchange
+		q.Name, // routing key
+		false,  // mandatory
+		false,  // immediate
+		amqp.Publishing {
+			ContentType: "text/plain",
+			Body:        []byte(body),
+		})
+	failOnError(err, "Failed to publish a message")
+
+}
+
+// receive
+func Test02(t *testing.T) {
+	conn, err := amqp.Dial("amqp://admin:admin@47.98.165.192:5672/my_vhost")
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+
+	q, err := ch.QueueDeclare(
+		"hello", // name
+		false,   // durable
+		false,   // delete when unused
+		false,   // exclusive
+		false,   // no-wait
+		nil,     // arguments
+	)
+	failOnError(err, "Failed to declare a queue")
+
+
+	msgs, err := ch.Consume(
+		q.Name, // queue
+		"",     // consumer
+		true,   // auto-ack
+		false,  // exclusive
+		false,  // no-local
+		false,  // no-wait
+		nil,    // args
+	)
+	failOnError(err, "Failed to register a consumer")
+
+	forever := make(chan bool)
+
+	go func() {
+		for d := range msgs {
+			log.Printf("Received a message: %s", d.Body)
+		}
+	}()
+
+	log.Printf(" [*] Waiting for messages. To exit press CTRL+C")
+	<-forever
+}
