fa2938370b2da8a073463fb3eda6ac89b4725163
taoey <hwt8080@163.com>
2019-12-30 23:27:43 +0800

    FEATURE 职责链模式（temp）--有点搞不懂golang继承的写法

---
+++ src/design_pattern/chain_of_responsibility/chain.go
@@ -0,0 +1,111 @@
+package chain_of_responsibility
+
+import "fmt"
+
+// 抽象审批者动作接口
+type IApprover interface {
+	SetSuccessor(approver *Approver)
+	ProcessRequest(request *PurchaseRequest)
+}
+
+// 采购单
+type PurchaseRequest struct {
+	Amount  float64 // 采购金额
+	Number  int     // 采购单编号
+	Purpose string  // 采购目的
+}
+
+// ----------------抽象审批者----------------------
+type Approver struct {
+	IApprover
+	Name string // 审批者姓名
+}
+
+// 抽象审批者构造函数
+func newApprover(name string) *Approver {
+	return &Approver{
+		Name: name,
+	}
+}
+
+// 设置后继者：所有具体审批者共用此方法
+func (this *Approver) SetSuccessor(approver IApprover) {
+	this.IApprover = approver
+}
+
+// 抽象请求处理方法：具体审批者需要单独设置审批流程
+func (this *Approver) ProcessRequest(request *PurchaseRequest) {}
+
+// ----------------------具体审批者：主任-------------------
+type Director struct {
+	*Approver
+}
+
+func newDirector(name string) *Director {
+	return &Director{
+		Approver: newApprover(name),
+	}
+}
+
+// 处理5万以下采购单
+func (this *Director) ProcessRequest(request *PurchaseRequest) {
+	if request.Amount < 50000 {
+		fmt.Println(fmt.Sprintf("主任%s审批采供但单：%+v", this.Name, request))
+	} else {
+		this.IApprover.ProcessRequest(request)
+	}
+}
+
+// ------------------具体审批者：副董事长----------------------
+type VicePresident struct {
+	*Approver
+}
+
+func newVicePresident(name string) *VicePresident {
+	return &VicePresident{
+		Approver: newApprover(name),
+	}
+}
+
+// 处理10万以下采购单
+func (this *VicePresident) ProcessRequest(request *PurchaseRequest) {
+	if request.Amount < 100000 {
+		fmt.Println(fmt.Sprintf("副董事长%s审批采供但单：%+v", this.Name, request))
+	} else {
+		this.IApprover.ProcessRequest(request)
+	}
+}
+
+// -------------------具体审批者：董事长-----------------------
+type President struct {
+	*Approver
+}
+
+func newPresident(name string) *Approver {
+	return newApprover(name)
+}
+
+// 处理50万以下采购单
+func (this *President) ProcessRequest(request *PurchaseRequest) {
+	if request.Amount < 500000 {
+		fmt.Println(fmt.Sprintf("董事长%s审批采供但单：%+v", this.Name, request))
+	} else {
+		this.IApprover.ProcessRequest(request)
+	}
+}
+
+// -------------------具体审批者：董事会----------------------
+type Congress struct {
+	*Approver
+}
+
+func newCongress(name string) *Approver {
+	return newApprover(name)
+}
+
+// 处理50万以上采购单
+func (this *Congress) ProcessRequest(request *PurchaseRequest) {
+	if request.Amount >= 5000000 {
+		fmt.Println(fmt.Sprintf("董事会%s审批采供但单：%+v", this.Name, request))
+	}
+}
---
+++ src/design_pattern/chain_of_responsibility/chain_test.go
@@ -0,0 +1,29 @@
+package chain_of_responsibility
+
+import (
+	"testing"
+)
+
+func TestChain(t *testing.T) {
+	user1 := newDirector("u1")
+	user2 := newVicePresident("u2")
+	user3 := newPresident("u3")
+	meeting := newCongress("meeting")
+
+	user1.SetSuccessor(user2)
+	user2.SetSuccessor(user3)
+	user3.SetSuccessor(meeting)
+
+	p1 := PurchaseRequest{45000, 100, "购买4.5万物品"}
+	user1.ProcessRequest(&p1)
+
+	p2 := PurchaseRequest{60000, 100, "购买6万物品"}
+	user1.ProcessRequest(&p2)
+
+	p3 := PurchaseRequest{160000, 100, "购买16万物品"}
+	user1.ProcessRequest(&p3)
+
+	p4 := PurchaseRequest{800000, 100, "购买80万物品"}
+	user1.ProcessRequest(&p4)
+
+}
