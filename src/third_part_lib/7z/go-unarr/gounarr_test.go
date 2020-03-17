8e459ff946870b67c72e1a7535df567f948ef42b
taoey <741494582@qq.com>
2020-03-17 11:45:30 +0800

    FEATURE 7z解压测试

--- src/third_part_lib/7z/go-unarr/gounarr_test.go
+++ src/third_part_lib/7z/go-unarr/gounarr_test.go
@@ -1,6 +1,7 @@
 package go_unarr
 
 import (
+	"fmt"
 	"github.com/gen2brain/go-unarr"
 	"testing"
 )
@@ -16,4 +17,29 @@ func Test01(t *testing.T) {
 	if err2 != nil {
 		panic(err2)
 	}
+}
+
+// 解压7z文件
+// dest 目录如果不存在，则会进行新建
+func Z7Extract(src,dest string) ([]string,error) {
+	archive, err := unarr.NewArchive(src)
+	defer archive.Close()
+
+	if err !=nil{
+		return nil,err
+	}
+	contents, err := archive.List()
+
+	extractErr := archive.Extract(dest)
+	if extractErr!=nil{
+		return nil,extractErr
+	}
+	return contents,nil
+}
+
+func TestZ7Extract(t *testing.T) {
+	src := "01.7z"
+	dest := "./"
+	contents, err := Z7Extract(src, dest)
+	fmt.Println(contents,err)
 }
