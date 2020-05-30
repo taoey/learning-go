625b8eeaaaadb87eb851eb6ce5bc528490aeaac6
taoey <741494582@qq.com>
2020-05-30 10:38:11 +0800

    PREF  更新LRU算法

--- src/go_algorithm/leetcode/146_LRU缓存/solution_test.go
+++ src/go_algorithm/leetcode/146_LRU缓存/solution_test.go
@@ -6,68 +6,68 @@ import (
 	"testing"
 )
 
-// LRU结构体
-type LRUCache struct {
-	capacity int
-	cache    map[int]*list.Element
-	list     *list.List
-}
-
-// key-value 结构体
+// 键值对
 type Pair struct {
 	key   int
 	value int
 }
 
-// 创建一个LRU对象
-func NewLRUCache(capacity int) *LRUCache {
-	return &LRUCache{
+type LRUCache struct {
+	capacity int
+	list     *list.List
+	cacheMap map[int]*list.Element
+}
+
+// 构造函数
+func Constructor(capacity int) LRUCache {
+	return LRUCache{
 		capacity: capacity,
 		list:     list.New(),
-		cache:    make(map[int]*list.Element),
+		cacheMap: make(map[int]*list.Element),
 	}
 }
 
-// 根据key获取value,没获取到返回-1
-// 通过map获取元素，获取到后把元素移动到头部
+// 获取数据
 func (this *LRUCache) Get(key int) int {
-	if item, ok := this.cache[key]; ok {
-		this.list.MoveToFront(item)
-		return item.Value.(Pair).value
+	if element, ok := this.cacheMap[key]; ok {
+		this.list.MoveToFront(element)
+		return element.Value.(Pair).value
 	}
 	return -1
 }
 
-// 放置一个key-value对
-// 需要注意容量放满时的情况
+// 放置数据
 func (this *LRUCache) Put(key int, value int) {
-	if elem, ok := this.cache[key]; ok {
-		this.list.MoveToFront(elem)
-		elem.Value = Pair{key, value}
+	// 判断是否在缓存中，已存在则直接调整数据位置,更新key-value
+	if element, ok := this.cacheMap[key]; ok {
+		this.list.MoveToFront(element)
+		element.Value = Pair{key, value}
 	} else {
+		// 判断缓存是否已满，已满则进行数据淘汰
 		if this.list.Len() >= this.capacity {
-			delete(this.cache, this.list.Back().Value.(Pair).key)
+			// 删除map中的数据
+			delete(this.cacheMap, this.list.Back().Value.(Pair).key)
+			// 删除list中的数据
 			this.list.Remove(this.list.Back())
 		}
-		this.list.PushFront(Pair{key, value})
-		this.cache[key] = this.list.Front()
+		front := this.list.PushFront(Pair{key, value})
+		this.cacheMap[key] = front
 	}
 }
 
 func TestLRU(t *testing.T) {
-	lruCache := NewLRUCache(3)
+	cache := Constructor(2)
 
-	lruCache.Put(1, 11)
-	lruCache.Put(2, 22)
-	lruCache.Put(3, 33)
-	lruCache.Put(4, 44)
+	cache.Put(1, 1)
+	cache.Put(2, 2)
+	cache.Get(1)             // 返回  1
+	cache.Put(3, 3)          // 该操作会使得关键字 2 作废
+	element1 := cache.Get(2) // 返回 -1 (未找到)
+	fmt.Println(element1)
+	cache.Put(4, 4)     // 该操作会使得关键字 1 作废
+	ele := cache.Get(1) // 返回 -1 (未找到)
+	fmt.Println(ele)
+	cache.Get(3) // 返回  3
+	cache.Get(4) // 返回  4
 
-	aa := lruCache.Get(3)
-	fmt.Println("aa:", aa)
-
-	bb := lruCache.Get(4)
-	fmt.Println("bb", bb)
-
-	cc := lruCache.Get(1)
-	fmt.Println("cc", cc)
 }
---
+++ src/go_algorithm/other/LRU缓存/solution_test.go
@@ -0,0 +1,73 @@
+package LRU缓存
+
+import (
+	"container/list"
+	"fmt"
+	"testing"
+)
+
+// LRU结构体
+type LRUCache struct {
+	capacity int
+	cache    map[int]*list.Element
+	list     *list.List
+}
+
+// key-value 结构体
+type Pair struct {
+	key   int
+	value int
+}
+
+// 创建一个LRU对象
+func NewLRUCache(capacity int) *LRUCache {
+	return &LRUCache{
+		capacity: capacity,
+		list:     list.New(),
+		cache:    make(map[int]*list.Element),
+	}
+}
+
+// 根据key获取value,没获取到返回-1
+// 通过map获取元素，获取到后把元素移动到头部
+func (this *LRUCache) Get(key int) int {
+	if item, ok := this.cache[key]; ok {
+		this.list.MoveToFront(item)
+		return item.Value.(Pair).value
+	}
+	return -1
+}
+
+// 放置一个key-value对
+// 需要注意容量放满时的情况
+func (this *LRUCache) Put(key int, value int) {
+	if elem, ok := this.cache[key]; ok {
+		this.list.MoveToFront(elem)
+		elem.Value = Pair{key, value}
+	} else {
+		if this.list.Len() >= this.capacity {
+			delete(this.cache, this.list.Back().Value.(Pair).key)
+			this.list.Remove(this.list.Back())
+		}
+		this.list.PushFront(Pair{key, value})
+		this.cache[key] = this.list.Front()
+	}
+}
+
+func TestLRU(t *testing.T) {
+	lruCache := NewLRUCache(3)
+
+	lruCache.Put(1, 11)
+	lruCache.Put(2, 22)
+	lruCache.Put(3, 33)
+	lruCache.Put(4, 44)
+
+	aa := lruCache.Get(3)
+	fmt.Println("aa:", aa)
+
+	bb := lruCache.Get(4)
+	fmt.Println("bb", bb)
+
+	cc := lruCache.Get(1)
+	fmt.Println("cc", cc)
+}
