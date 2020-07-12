f4ff677bcd83431164b00391326014fd18697c7d
taoey <741494582@qq.com>
2020-07-12 21:50:49 +0800

    PREF 使用struct作为自定义set的value

--- src/base/map/map_test.go
+++ src/base/map/map_test.go
@@ -5,9 +5,9 @@ import (
 	"testing"
 )
 
-// 使用map[string]bool来模拟 set
+// 使用map[string]struct{}来模拟 set
 func Test01(t *testing.T) {
-	set := make(map[string]bool)
+	set := make(map[string]struct{})
 
 	result := set["hello"]
 	fmt.Println(result)
